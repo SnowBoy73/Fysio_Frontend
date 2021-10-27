c45e92220f0835f810250821279a3cf0a5a93387
TriggerStorm <michael.h.p@hotmail.com>
2021-10-27 18:50:59 +0200

    working on home component
    css and html for home component img slide show

--- Fysio-Frontend/src/app/home/home.component.ts
+++ Fysio-Frontend/src/app/home/home.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+   carouselSlide = document.querySelector('.carousel-slide');
+   carouselImages = document.querySelector('.carousel-slide img');
+   // Buttons
+
+   prevBtn = document.querySelector('#prevBtn');
+   nextBtn = document.querySelector('#nextBtn');
+
+  //counter
+   counter = 1;
+   size: undefined;
   constructor() { }
 
+
   ngOnInit(): void {
+    if(this.carouselImages != null){
+      let size: number;
+      // @ts-ignore
+      size = this.carouselImages[0].clientWidth;
+    }
+
+
   }
+
+
 
 }
