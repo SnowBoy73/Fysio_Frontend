daf3df8a3f40427ee9d4d4c739cd2d01cd1e5428
TriggerStorm <michael.h.p@hotmail.com>
2022-01-06 16:36:03 +0100

    price list update final
    png phone size

--- src/app/pricelist/pricelist.component.ts
+++ src/app/pricelist/pricelist.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pricelist.component.scss']
 })
 export class PricelistComponent implements OnInit {
+  dataSource: any;
 
   constructor() { }
 
